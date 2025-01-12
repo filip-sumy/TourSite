c900e0ac51aa2b11fd26cd7131f85427ab1a3fb3
Philip Kokurenchuk <filip-sumy@ukr.net>
2025-01-12 11:39:12 +0200

    1.9

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const slider1 = new Swiper('.card-wrapper', {
   spaceBetween: 0,
   slidesPerView: 1,
   initialSlide: 1,
+  freeMode: true,
   // If we need pagination
   pagination: {
     el: '.card-wrapper .swiper-pagination',
@@ -123,6 +124,7 @@ const slider2 = new Swiper('.reviews-wrapper', {
   spaceBetween: 0,
   slidesPerView: 1,
   initialSlide: 0,
+  freeMode: true,
   // If we need pagination
   pagination: {
     el: '.reviews-wrapper .swiper-pagination',
@@ -243,7 +245,8 @@ const slider3 = new Swiper('.photos-wrapper', {
   spaceBetween: 0,
   slidesPerView: 1,
   initialSlide: 0,
-
+  freeMode: true,
+  
   // Pagination
   pagination: {
       el: '.photos-wrapper .swiper-pagination',
@@ -359,7 +362,115 @@ const slider3 = new Swiper('.photos-wrapper', {
 }
 });
 
-
+const slider4 = new Swiper('.question-wrapper', {
+  
+  // Breakpoints for responsiveness
+  breakpoints: {
+    0 : {
+      allowTouchMove: false,
+      freeMode: false,
+      loop: false,
+      //initialSlide: 0,
+    },
+    // 375 : {
+    //   slidesPerView: 1.2,
+    //   spaceBetween: 30,
+    // },
+    
+    // 420: {
+    //   slidesPerView: 1.4,
+    //   spaceBetween: 50,
+    // },
+    // 470: {
+    //   slidesPerView: 1.6,
+    //   spaceBetween: 50,
+    // },
+    // 520: {
+    //   slidesPerView: 1.8,
+    //   spaceBetween: 50,
+    // },
+    600: {
+      allowTouchMove: true,
+      freeMode: true,
+      loop: true,
+      //initialSlide: 0,
+      slidesPerView: 1.8,
+      spaceBetween: 30,
+    },
+    640: {
+      slidesPerView: 1.8,
+      spaceBetween: 10,
+    },
+    670: {
+      slidesPerView: 1.8,
+      spaceBetween: 0,
+    },
+    700: {
+      slidesPerView: 1.9,
+      spaceBetween: -70,
+    },
+    720: {
+      slidesPerView: 1.9,
+      spaceBetween: -50,
+    },
+    740: {
+      slidesPerView: 2,
+      spaceBetween: -20,
+    },
+    768 : {
+        slidesPerView: 2.2,
+        spaceBetween: 0, 
+    },
+    820 : {
+      slidesPerView: 2.4,
+      spaceBetween: 0, 
+    },
+    870 : {
+      slidesPerView: 2.6,
+      spaceBetween: 0, 
+    },
+    920 : {
+      slidesPerView: 2.8,
+      spaceBetween: 0, 
+    },
+    970 : {
+      slidesPerView: 2.8,
+      spaceBetween: -10, 
+    },
+    1000 : {
+      slidesPerView: 2.8,
+      spaceBetween: -30, 
+    },
+    1060 : {
+      slidesPerView: 2.8,
+      spaceBetween: -50, 
+    },
+    1100 : {
+      slidesPerView: 2.8,
+      spaceBetween: -70, 
+    },
+    1130 : {
+      slidesPerView: 2.9,
+      spaceBetween: -70, 
+    },
+    1150 : {
+      slidesPerView: 2.9,
+      spaceBetween: -100, 
+    },
+    1200 : {
+      slidesPerView: 2.9,
+      spaceBetween: -170,
+    },
+    1250 : {
+      slidesPerView: 3,
+      spaceBetween: -320,
+    },
+    1400: {
+      slidesPerView: 4,
+      spaceBetween: 60,
+  }
+}
+});
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -368,10 +479,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const main = document.getElementById("main");
     const menuButton = document.getElementById("menuButton");
     const menuIcon = document.getElementById("menuIcon");
-
+    const footer = document.getElementById("footer");
     function toggleMenu() {
         menu.classList.toggle("hidden");
         main.classList.toggle("hidden");
+        footer.classList.toggle("hidden");
         body__container.classList.toggle("hidden");
         // Изменяем иконку и alt текст в зависимости от состояния меню
         if (menu.classList.contains("hidden")) {
