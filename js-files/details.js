63432b318f41ddf860ecb186a113313c87aed704
Philip Kokurenchuk <filip-sumy@ukr.net>
2025-01-21 13:55:20 +0200

    2.6

--- js-files/details.js
+++ js-files/details.js
@@ -25,3 +25,113 @@ document.addEventListener("DOMContentLoaded", () => {
     // Добавляем обработчик события на кнопку
     menuButton.addEventListener("click", toggleMenu);
 });
+
+const slider4 = new Swiper('.question-wrapper', {
+  
+    // Breakpoints for responsiveness
+    breakpoints: {
+      0 : {
+        allowTouchMove: false,
+        freeMode: false,
+        loop: false,
+        //initialSlide: 0,
+      },
+      375 : {
+        slidesPerView: 1.2,
+        spaceBetween: 30,
+      },
+      
+      420: {
+        slidesPerView: 1.4,
+        spaceBetween: 50,
+      },
+      470: {
+        slidesPerView: 1.6,
+        spaceBetween: 50,
+      },
+      520: {
+        slidesPerView: 1.8,
+        spaceBetween: 50,
+      },
+      600: {
+        allowTouchMove: true,
+        freeMode: true,
+        loop: true,
+        //initialSlide: 0,
+        slidesPerView: 1.8,
+        spaceBetween: 30,
+      },
+      640: {
+        slidesPerView: 1.8,
+        spaceBetween: 10,
+      },
+      670: {
+        slidesPerView: 1.8,
+        spaceBetween: 0,
+      },
+      700: {
+        slidesPerView: 1.9,
+        spaceBetween: -70,
+      },
+      720: {
+        slidesPerView: 1.9,
+        spaceBetween: -50,
+      },
+      740: {
+        slidesPerView: 2,
+        spaceBetween: -20,
+      },
+      768 : {
+          slidesPerView: 2.2,
+          spaceBetween: 0, 
+      },
+      820 : {
+        slidesPerView: 2.4,
+        spaceBetween: 0, 
+      },
+      870 : {
+        slidesPerView: 2.6,
+        spaceBetween: 0, 
+      },
+      920 : {
+        slidesPerView: 2.8,
+        spaceBetween: 0, 
+      },
+      970 : {
+        slidesPerView: 2.8,
+        spaceBetween: -10, 
+      },
+      1000 : {
+        slidesPerView: 2.8,
+        spaceBetween: -30, 
+      },
+      1060 : {
+        slidesPerView: 2.8,
+        spaceBetween: -50, 
+      },
+      1100 : {
+        slidesPerView: 2.8,
+        spaceBetween: -70, 
+      },
+      1130 : {
+        slidesPerView: 2.9,
+        spaceBetween: -70, 
+      },
+      1150 : {
+        slidesPerView: 2.9,
+        spaceBetween: -100, 
+      },
+      1200 : {
+        slidesPerView: 2.9,
+        spaceBetween: -170,
+      },
+      1250 : {
+        slidesPerView: 3,
+        spaceBetween: -320,
+      },
+      1400: {
+        slidesPerView: 4,
+        spaceBetween: 60,
+    }
+  }
+  });
