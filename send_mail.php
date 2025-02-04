0baefd0723c7efe3f02be027f17850a1dfc37276
Yaroslav <khodun.yaroslav.qa@gmail.com>
2025-02-04 22:31:00 +0200

    send_mail file are configured

--- send_mail.php
+++ send_mail.php
@@ -1,26 +1,48 @@
 <?php
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    require "PHPMailer/src/PHPMailer.php";
-    require "PHPMailer/src/Exception.php";
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 
-    $mail = new PHPMailer(true);
-    $mail->CharSet = "UTF-8";
 
-    $name = $_POST["name"];
+$name = $_POST['user_name'];
+$phone = $_POST['user_phone'];
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     
-    $phone = $_POST["phone"];
-    
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Username   = 'homiewlker@gmail.com';                     //SMTP username
+    $mail->Password   = 'cmii ygwh zjjy vfus';                               //SMTP password
 
-    $body = $name . ' ' . $phone;
-    $theme = "[Заявка с формы]";
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    $mail->addAddress("filip-sumy@ukr.net");
+    //Recipients
+    $mail->setFrom('yaroslavkhodun@gmail.com', 'Mailer');
+    $mail->addAddress('yaroslavkhodun@gmail.com', 'Joe User');     //Add a recipient
 
-    $mail->Subject = $theme;
-    $mail->Body = $body;
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Заявка с формы';
+    $mail->Body    = '
+        <h4>Name : '.$name.'</h4>
+        <h4>Phone : '.$phone.'</h4>
+    ';
 
     $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 
+?>
