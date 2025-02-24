33e59aff9abdbc1777780091f8875ec004606491
Philip Kokurenchuk <filip-sumy@ukr.net>
2025-02-24 12:11:03 +0200

    menuButton on booking.php is repaired

--- js-files/booking.js
+++ js-files/booking.js
@@ -1,8 +1,32 @@
-const addButton = document.getElementById("add__button");
-const addContainer = document.getElementById("addContainer");
+document.addEventListener("DOMContentLoaded", () => {
+    const menu = document.getElementById("menu");
+    const body__container = document.getElementById("body__container");
+    const main = document.getElementById("main");
+    const menuButton = document.getElementById("menuButton");
+    const menuIcon = document.getElementById("menuIcon");
+    const footer = document.getElementById("footer");
+    const header__container = document.getElementById("header__container");
+    function toggleMenu() {
+        menu.classList.toggle("hidden");
+        main.classList.toggle("hidden");
+        footer.classList.toggle("hidden");
+        body__container.classList.toggle("hidden");
+        header__container.classList.toggle("hidden");
+        // Изменяем иконку и alt текст в зависимости от состояния меню
+        if (menu.classList.contains("hidden")) {
+            menuIcon.src = "../images/Menu_button.svg";
+            menuIcon.alt = "Открыть меню";
+        } else {
+            menuIcon.src = "../images/Menu_button_back.svg";
+            menuIcon.alt = "Закрыть меню";
+        }
+    }
 
-let counter = 1;
-if (addButton && addContainer) {
+    const addButton = document.getElementById("add__button");
+    const addContainer = document.getElementById("addContainer");
+
+    let counter = 1;
+    if (addButton && addContainer) {
     addButton.addEventListener("click", (event) => {
         event.preventDefault();
         counter++;
@@ -48,4 +72,7 @@ if (addButton && addContainer) {
         // Добавляем контейнер пассажира в конец addContainer
         addContainer.appendChild(passengerContainer);
     });
-}
+    }
+    // Добавляем обработчик события на кнопку
+    menuButton.addEventListener("click", toggleMenu);
+});
